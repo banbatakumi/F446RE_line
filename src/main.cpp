a28bcce942940fc4b8a7160e833c427298e12bf2
banbatakumi <bantech0210@gmail.com>
2023-08-28 20:49:07 +0900

    エンコーダーテスト

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,40 @@ DigitalOut led_1(PC_10);
 DigitalOut led_2(PC_11);
 DigitalOut led_3(PC_12);
 
+AnalogIn encoder_1(PC_3);
+
+uint8_t value, pre_value;
+uint16_t m_count, pre_count;
+uint8_t speed;
+
+Timer test;
+
 // グローバル変数定義
 
 int main() {
-      main_mcu.baud(38400);   // 通信速度: 9600, 14400, 19200, 28800, 38400, 57600, 115200
-      main_mcu.attach(main_mcu_rx, Serial::RxIrq);   // シリアル割り込み
+      main_mcu.baud(9600);   // 通信速度: 9600, 14400, 19200, 28800, 38400, 57600, 115200
+      // main_mcu.attach(main_mcu_rx, Serial::RxIrq);   // シリアル割り込み
+
+      test.start();
 
       while (1) {
+            pre_value = value;
+            value = encoder_1.read_u16() / 256;
+            if (value > 40 && pre_value <= 40) {
+                  m_count += 1;
+            }
+            if (value < 40 && pre_value >= 40) {
+                  m_count += 1;
+            }
+            if (m_count > 250) {
+                  m_count = 0;
+            }
+            if (test.read() > 0.1) {
+                  speed = m_count - pre_count;
+                  pre_count = m_count;
+                  test.reset();
+            }
+            main_mcu.putc(speed);
       }
 }
 
