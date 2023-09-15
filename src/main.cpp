2653668da0d58e7b906f5689d8bebdfdd4d70c46
banbatakumi <bantech0210@gmail.com>
2023-09-15 17:47:24 +0900

    左右のライン追加

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "mbed.h"
 
 // UART通信定義
-Serial main_mcu(PC_6, PC_7);   // TX, RX
+RawSerial main_mcu(PC_6, PC_7);   // TX, RX
 
 // 関数定義
 void main_mcu_rx();
@@ -12,24 +12,32 @@ encoder Encoder(PC_4, PA_7, PC_3, PA_6);
 DigitalOut led_1(PC_10);
 DigitalOut led_2(PC_11);
 DigitalOut led_3(PC_12);
+AnalogIn line_left(PC_2);
+AnalogIn line_right(PB_1);
 
 // グローバル変数定義
+uint8_t line_left_val;
+uint8_t line_right_val;
 
 Timer uart_timer;
 
 int main() {
-      main_mcu.baud(38400);   // 通信速度: 9600, 14400, 19200, 28800, 38400, 57600, 115200
+      main_mcu.baud(9600);   // 通信速度: 9600, 14400, 19200, 28800, 38400, 57600, 115200
       // main_mcu.attach(main_mcu_rx, Serial::RxIrq);   // シリアル割り込み
 
       uart_timer.start();
 
       while (1) {
+            line_left_val = line_left.read_u16() / 256;
+            line_right_val = line_right.read_u16() / 256;
             Encoder.read();
             main_mcu.putc('H');
             main_mcu.putc(Encoder.sensor_1());
             main_mcu.putc(Encoder.sensor_2());
             main_mcu.putc(Encoder.sensor_3());
             main_mcu.putc(Encoder.sensor_4());
+            main_mcu.putc(line_left_val);
+            main_mcu.putc(line_right_val);
       }
 }
 
